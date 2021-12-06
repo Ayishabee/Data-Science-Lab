5faa4cf94daa5ab60fc5c6e714efe551abdd4daf
Ayishabee <95555890+Ayishabee@users.noreply.github.com>
2021-12-06 10:32:51 +0530

    Add files via upload

---
+++ pgm7.py
@@ -0,0 +1,11 @@
+from datetime import date,timedelta
+dt=date.today()-timedelta(5)
+print('Current Date:',date.today())
+print('5 days before current date:',dt)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
